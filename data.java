7353bd508592152796c828bc6f2a3d086630bf0f
divyaansh <parmardivyaansh8@gmail.com>
2026-01-16 14:54:27 +0530

    dsf

---
+++ data.java
@@ -0,0 +1,8 @@
+public class data {
+    public static void main(String[] args) {
+        System.out.println("My name is Divyaansh parmar");
+        System.out.println("my prn is: 24070122130");
+        System.out.println("address is teerth avila,sus gaon");
+    }
+    
+}
