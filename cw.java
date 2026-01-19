4686007360a2b77eb8f1e7a60bf18dbc6a9a5652
divyaansh <parmardivyaansh8@gmail.com>
2026-01-19 10:35:18 +0530

    dsdfs

---
+++ cw.java
@@ -0,0 +1,14 @@
+class person{
+    String name;
+    void display(){
+        System.out.println("Name: "+name);
+    }
+}
+public class cw {
+    public static void main(String[] args) {
+        person p=new person();
+        p.name ="john";
+        p.display();
+    }
+    
+}
